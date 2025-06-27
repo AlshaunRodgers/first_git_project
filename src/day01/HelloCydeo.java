ce7b880029b286c2b64527206ca70ee08590bbc0
alsha <alshaun.rodgers@gmail.com>
2025-06-27 14:58:18 -0700

    HelloCydeo Updated

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -4,7 +4,7 @@ public class HelloCydeo {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Cydeo Class");
+        System.out.println("Hello Cydeo");
 
         // Some changes are added.
     }
